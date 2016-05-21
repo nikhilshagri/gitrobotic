f89f463cc455ff0922cfdc0264f5bdf6a297d759
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-05-21 20:44:40 +0530

    Function: Can fetch all commits' info from master branch from specified repo

--- src/main.js
+++ src/main.js
@@ -4,45 +4,142 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-import ActivityArea from './ActivityArea'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
 
 import Git from 'nodegit';
 
-var pathToRepo = require('path').resolve('../js-reporters');
+// var pathToRepo = require('path').resolve('../js-reporters');
 
-var getMostRecentCommit = function(repository) {
-  return repository.getBranchCommit("master");
-};
 
-var getCommitMessage = function(commit) {
-  return commit.message();
-};
+var getCommits = function(pathToRepo) {
 
-Git.Repository.open(pathToRepo).then(getMostRecentCommit)
-.then(getCommitMessage)
-.then(function(message) {
-  console.log(message);
-})
-.catch(function(error) {
-  console.log("ERROR: "+error);
-});
+  pathToRepo = require('path').resolve(pathToRepo);
+  Git.Repository.open(pathToRepo)
+  .then(function(repo) {
+    return repo.getMasterCommit();
+  })
+  .then(function( firstMasterCommit ) {
+    var history = firstMasterCommit.history( Git.Revwalk.SORT.Time);
+    history.on("end", walk);
+    history.start();
+  })
+  .done();
 
+  //   function passed to history.on() listener to receive commits' info. 
+  //   calls the setCommitState of App class to set new state
+  var walk = (function(commitsArr) {
+    var commits = [];
+      commitsArr.forEach( function(commit) {
+        commits.push({ 
+          sha: commit.sha(), 
+          author: commit.author().name(),
+          email: commit.author().email(),
+          date: commit.date().toString(),
+          message: commit.message()
+        });
+      });
+
+      this.setCommitsState(commits);
+  }).bind(this);
+
+}
 
 const darkMuiTheme = getMuiTheme(darkBaseTheme);
+const style= {
+  margin:12,
+};
 
-class App extends React.Component {
+class Commit extends React.Component {
   render() {
+    var commit = this.props.commit;
     return (
-      <MuiThemeProvider muiTheme={darkMuiTheme}>
-         <div>
-           <RaisedButton label="Deflt" />
-           <div> qwerty </div>
-         </div>
-      </MuiThemeProvider>
+      <div>
+        <Card >
+          <CardHeader subtitle={commit.sha} />
+          <CardTitle title={commit.author} subtitle={commit.email} />
+          <CardTitle subtitle={commit.date} />
+          <CardText>
+            {commit.message}
+          </CardText>
+        </Card>
+        <br />
+      </div>
     )
   }
 }
 
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={ value:props.pathToRepo };
+    this.updateValue = this.updateValue.bind(this);
+    this.sendQuery = this.sendQuery.bind(this);
+  }
+
+  updateValue(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  sendQuery() {
+    this.props.onKeyPress(this.state.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="Enter Repo name here..." ref="textField" value={this.state.value} onChange={this.updateValue} />
+        <RaisedButton label="Get Commits!" style={style} onMouseDown={this.sendQuery} />
+      </div>
+    )
+  }
+}
+
+class CommitTable extends React.Component {
+  render() {
+    var rows=[];
+    this.props.commits.forEach(function(commit, index) {
+      rows.push(<Commit commit={commit} key={index} />);
+    });
+    return (
+    <div>{ rows }</div>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { pathToRepo:'' , commits: [] };
+    this.handleKeyPressChange = this.handleKeyPressChange.bind(this);
+  }
+
+  handleKeyPressChange(passedPath)
+  {
+    (getCommits.bind(this))(passedPath);
+  }
+
+  setCommitsState(commits)
+  {
+    this.setState( { commits: commits });
+    console.log(this.state.commits);
+  }
+
+  render() {
+
+    return (
+      <MuiThemeProvider muiTheme={darkMuiTheme}>
+        <div>
+          <SearchBar pathToRepo={this.state.pathToRepo} onKeyPress={this.handleKeyPressChange} />  
+          <CommitTable commits={this.state.commits} />
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+
+}
 
 ReactDOM.render(
   <App />,
---
+++ src/test-on-node.js
@@ -0,0 +1,5 @@
+var arr = [];
+arr.push('qwreyy');
+arr.push('ghjkl');
+arr.push('tyuiop');
+console.log(arr[1]);
