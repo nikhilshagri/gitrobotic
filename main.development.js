5fa951a4b22cbc7101bf534e5e49d46ab14425b5
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-05-31 19:25:04 +0530

    View: Reset size of MainWindow

--- main.development.js
+++ main.development.js
@@ -10,8 +10,20 @@ if (process.env.NODE_ENV === 'development') {
 
 app.on( 'ready',function() {
   var mainWindow = new BrowserWindow({
+	  show: false,
+    width: 1300,
+    height: 900
   });
    mainWindow.loadURL('file://' + __dirname + '/src/index-electron.html');
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.show();
+    mainWindow.focus();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 
   if (process.env.NODE_ENV === 'development') {
     mainWindow.openDevTools();
