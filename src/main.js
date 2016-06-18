816faab81f784ac82792092a61dc9f6621cb63df
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-06-18 23:50:06 +0530

    View: Removed repoPanel code and added its import object

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ import Snackbar from 'material-ui/Snackbar';
 
 import CommitTree from './CommitTreePanel';
 import StagingArea from './StagingAreaPanel';
+import Repo from './RepoPanel';
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 import injectTapEventPlugin from 'react-tap-event-plugin';
