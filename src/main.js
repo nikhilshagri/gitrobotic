93919c7b2163bb3716c38d4771f50d3c3aea6a16
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-06-24 02:55:21 +0530

    Core: Added react-router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link } from 'react-router'
+
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
