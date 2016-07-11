b0cdc311215ce9bb63bc49b01823b79ba521e521
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-07-11 23:33:23 +0530

    Frontend: added collectCheckedLines function

--- src/utils/StageSelective.js
+++ src/utils/StageSelective.js
@@ -14,12 +14,41 @@ class StageSelective extends React.Component {
   createCommit = (commitMsg) => {
     console.log(commitMsg);
     console.log(this.diffPanel);
+  collectCheckedLines = (commitMsg) => {
+
+    const LINESTATUS = {
+      UNMODIFIED: 32,
+      ADDED: 43,
+      DELETED: 45,
+    };
+    const checked = this.diffPanel.state.checked;
+    const diffsArr = this.diffPanel.state.diffsArr;
+
+    let filesArr = [];
+    diffsArr.forEach( (diffFile, fileIndex) => {
+
+      let linesArr = [];
+      diffFile.diffs.forEach( (diff, diffIndex) => {
+        diff.lines.forEach( (line, lineIndex) => {
+
+          // only accept checked and modified lines
+          if(checked[fileIndex][diffIndex][lineIndex] &&
+          line.origin() !== LINESTATUS.UNMODIFIED)
+            linesArr.push(line);
+        });
+      });
+
+      if(linesArr.length !== 0)
+        filesArr.push({lines: linesArr, path: diffFile.path.old});
+    });
+
+    this.createCommit(filesArr);
   }
 
   render = () => {
     return (
       <div>
-        <CommitMessage buttonMsg='Commit selected lines!' commitCB={this.createCommit} />
+        <CommitMessage buttonMsg='Commit selected lines!' commitCB={this.collectCheckedLines} />
         <DiffPanel {...this.props} showSelect={true} ref={(ref) => this.diffPanel = ref} />
       </div>
     );
