7ea65dd5a6d6562dccd0b9fd2c35312a5ee438c5
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-07-29 22:41:14 +0530

    Misc: Added electron devtools installer

--- main.development.js
+++ main.development.js
@@ -3,12 +3,18 @@
 var app =require('app');
 var BrowserWindow = require('browser-window');
 var path = require( "path" );
+import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 
 if (process.env.NODE_ENV === 'development') {
   require('electron-debug')(); // eslint-disable-line global-require
 }
 
 app.on( 'ready',function() {
+
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
+
   var mainWindow = new BrowserWindow({
     width: 1200,
     height: 600
