17cbfbf8a3853a7a3f47cf8b7850873048be2e1a
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-07-30 19:49:21 +0530

    Updated Electron version to be compatible with devtools extension

--- main.development.js
+++ main.development.js
@@ -1,9 +1,9 @@
 //electron entry-point
 
-var app =require('app');
-var BrowserWindow = require('browser-window');
-var path = require( "path" );
 import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+var app = require('electron').app;
+var BrowserWindow = require('electron').BrowserWindow;
+var path = require( "path" );
 
 if (process.env.NODE_ENV === 'development') {
   require('electron-debug')(); // eslint-disable-line global-require
@@ -11,9 +11,9 @@ if (process.env.NODE_ENV === 'development') {
 
 app.on( 'ready',function() {
 
-  installExtension(REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
+installExtension(REACT_DEVELOPER_TOOLS)
+  .then((name) => console.log(`Added Extension:  ${name}`))
+  .catch((err) => console.log('An error occurred: ', err));
 
   var mainWindow = new BrowserWindow({
     width: 1200,
