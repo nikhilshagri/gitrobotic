c19af0e6c515dcd74a4d7bef8b25b81c401f803b
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-09-08 04:45:05 +0000

    Added icon to the app

--- main.start.js
+++ main.start.js
@@ -2,10 +2,13 @@
 var app = require('electron').app;
 var BrowserWindow = require('electron').BrowserWindow;
 var path = require( "path" );
+const {nativeImage} = require('electron');
 
 if (process.env.NODE_ENV === 'development') {
   require('electron-debug')(); // eslint-disable-line global-require
 }
+
+const gitrobotic_icon = nativeImage.createFromPath('./gitrobotic.png');
 
 app.on('ready',function() {
 
@@ -19,7 +22,8 @@ app.on('ready',function() {
 
   var mainWindow = new BrowserWindow({
     width: 1200,
-    height: 600
+    height: 600,
+    icon: gitrobotic_icon
   });
   mainWindow.loadURL('file://' + __dirname + '/src/index-electron.html');
 
